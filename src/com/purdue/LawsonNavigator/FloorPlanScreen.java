58f988f3766d4a33369f16be8f09b35411d020eb
George B. Brinzea <gbrinzea@moore21.cs.purdue.edu>
2011-11-04 17:23:44 -0400

    Updating client

--- src/com/purdue/LawsonNavigator/FloorPlanScreen.java
+++ src/com/purdue/LawsonNavigator/FloorPlanScreen.java
@@ -59,6 +59,56 @@ public class FloorPlanScreen extends Activity {
     			saved.setFloor(Floor.BASEMENT);
     			saved.setUsage(Transport.ELEVATOR);
     			
+    			//Sending stuff to server
+    			Socket kkSocket = null;
+			PrintWriter out = null;
+			BufferedReader in = null;
+			ObjectInputStream ois = null;
+			ObjectOutputStream oos = null;
+	
+			//The IP address of moore01	
+			byte[] IP = new byte[4];
+			IP[0] = (byte) 128;
+			IP[1] = (byte) 10;
+			IP[2] = (byte) 12;
+			IP[3] = (byte) 131;
+
+			try {
+			    kkSocket = new Socket(InetAddress.getByAddress(IP), 4444);
+			    out = new PrintWriter(kkSocket.getOutputStream(), true);
+			    in = new BufferedReader(new InputStreamReader(kkSocket.getInputStream()));
+				ois = new ObjectInputStream(kkSocket.getInputStream());
+				oos = new ObjectOutputStream(kkSocket.getOutputStream());
+			} catch (UnknownHostException e) {
+			    System.err.println("Cannot find the host.");
+			    System.exit(1);
+			} catch (IOException e) {
+			    System.err.println("Couldn't get I/O for the connection to the host.");
+			    System.exit(1);
+			}
+			
+			try{
+				oos.writeObject(getRoomMap);
+			}catch(Exception e){
+				System.out.println("oos error");
+				e.printStackTrace();
+				System.exit(1);
+			}
+
+    			//Toast.makeText(getApplicationContext(), in.readLine(), Toast.LENGTH_SHORT).show();
+    			
+    			try {
+				out.close();
+				in.close();
+				oos.close();
+				ois.close();
+				kkSocket.close();
+			}catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+    			
+    			
     			Toast.makeText(getApplicationContext(), finalFloor + " MAP!", Toast.LENGTH_SHORT).show();
     			/*Intent i = new Intent();
 				i.setClassName("com.LawsonNavigator.org", "com.LawsonNavigator.org.LawsonNavigatorActivity");
